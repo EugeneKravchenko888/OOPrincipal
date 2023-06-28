9eea439badc8502be215b72e080df7f65c275b0f
Jhonni test <Jhonni888@yandex.ru>
2023-06-28 21:09:42 +0300

    Initial

---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.javaqa;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле паспорт
+    public String patronymic; // поле отчество
+    public String phone; // поле телефон
+    public String surname; // поле фамилия
+    public Boolean subscription; // поле подписка да/ нет
+    public FormDate birthday; // днюха
+
+}
